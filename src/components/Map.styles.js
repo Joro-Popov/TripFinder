675da601c6fb4239840790a28c81e5c7d88f7446
Joro-Popov <georgi.popov@motion-software.com>
2021-09-19 15:05:31 +0300

    Installed react-native-maps

--- src/components/Map.styles.js
+++ src/components/Map.styles.js
@@ -3,15 +3,6 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  container: {
-    width: '100%',
-    height: '100%',
-  },
-  map: {
-    flex: 1
   }
 });
 
--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,34 +1,20 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 import styles from './Map.styles';
-import MapboxGL from '@react-native-mapbox-gl/maps';
-
-MapboxGL.setAccessToken('pk.eyJ1IjoicG9wb3Y5MzciLCJhIjoiY2t0cG9zdHA0MDczYjJ1cW80ejE4N3ZvZyJ9.DcC4k_D8z4mVQeKrwmjY-g')
+import MapView, { AnimatedRegion, Animated } from 'react-native-maps';
 
 export default function MapComponent() {
     return (
     <View style={styles.page}>
-        <View style={styles.container}>
-            <MapboxGL.UserLocation 
-                animated={true}
-                visible={true}
-            />
-            <MapboxGL.MapView
-                style={styles.map}
-                zoomEnabled={true}
-                rotateEnabled={true}
-                zoomLevel={9}
-            >
-                <MapboxGL.UserLocation 
-                    animated={true}
-                    visible={true}
-                />
-                <MapboxGL.Camera defaultSettings={{
-                    centerCoordinate: [23.094304,42.582637,23.565686,42.784036],
-                    zoomLevel: 6,
-                }} followUserLocation />
-            </MapboxGL.MapView>
-        </View>
+         <MapView
+            style={{flex: 1}}
+            initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+            }}
+        />
     </View>
     )
 }
