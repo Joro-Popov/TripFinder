e0529445685129e0933af46726e66c6f6d9d2ec4
Joro-Popov <georgi.popov@motion-software.com>
2021-12-22 13:44:26 +0200

    Configure ios dependencies

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import {View, Platform} from 'react-native';
 import styles from './Map.styles';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 
-const GOOGLE_MAPS_APIKEY = Platform.OS === 'android' ? 'AIzaSyBF18IstK-OTQ_Cq_XS46Z9z1jpRWNRfc8' : 'AIzaSyCIdH8gqxAWpk2EZ8arv7hvxZdb4bUwCPk';
+const GOOGLE_MAPS_APIKEY =
+  Platform.OS === 'android'
+    ? 'AIzaSyBF18IstK-OTQ_Cq_XS46Z9z1jpRWNRfc8'
+    : 'AIzaSyCIdH8gqxAWpk2EZ8arv7hvxZdb4bUwCPk';
 
 export default function MapComponent() {
-    const origin = {latitude: 37.3318456, longitude: -122.0296002};
-    const destination = {latitude: 37.771707, longitude: -122.4053769};
+  const origin = {latitude: 37.3318456, longitude: -122.0296002};
+  const destination = {latitude: 37.771707, longitude: -122.4053769};
 
-    return (
-        <View style={styles.page}>
-            <MapView 
-                style={styles.page}
-                minZoomLevel={11}
-                initialRegion={{
-                latitude: 37.8025259,
-                longitude: -122.4351431,
-                latitudeDelta: 0.40,
-                longitudeDelta: 0.40,
-                }}>
-                <Marker coordinate={origin}></Marker>
-                <Marker coordinate={destination}></Marker>
-                <MapViewDirections
-                    origin={origin}
-                    destination={destination}
-                    apikey={GOOGLE_MAPS_APIKEY}
-                />
-            </MapView>
-        </View>
-    )
+  return (
+    <View style={styles.page}>
+      <MapView
+        style={styles.page}
+        minZoomLevel={11}
+        initialRegion={{
+          latitude: 37.8025259,
+          longitude: -122.4351431,
+          latitudeDelta: 0.4,
+          longitudeDelta: 0.4,
+        }}>
+        <Marker coordinate={origin} />
+        <Marker coordinate={destination} />
+        <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_MAPS_APIKEY}
+        />
+      </MapView>
+    </View>
+  );
 }
